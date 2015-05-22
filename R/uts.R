8291a3f0c39be4f0808a8bf9da6726dbffc17fdd
Andreas Eckner <andreas@eckner.com>
2015-05-22 14:48:57 -0400

    Added "uts" constructor, including documentation

--- R/uts.R
+++ R/uts.R
@@ -2,14 +2,42 @@
 # UTS (Unevenly-spaced Time Series) S3 class #
 # ############################################
 
-
-
 # -----------------
 # Generic functions
 # -----------------
 
 
-
 # ----------------------
 # Method implementations
 # ----------------------
+
+#' Unevenly-spaced Time Series
+#' 
+#' Create an unvenly-spaced time series (\code{"uts"}) object from a vector of observation values and a vector of observation times of matching length.
+#'
+#' @return An object of class \code{"uts"}.
+#' @param values a vector of observation values.
+#' @param times a vector of observation times. Must be of class \code{"POSIXct"} or be coercible using \code{\link{as.POSIXct}}.
+#' 
+#' @examples
+#' # Create a numeric "uts"
+#' dts <- c("2007-11-08", "2007-11-08", "2007-11-08", "2007-11-09", "2007-11-09", "2007-11-09")
+#' tms <- c("7:00:00", "8:01:00", "13:15:00", "7:30:00", "8:51:00", "15:15:00")
+#' uts(values=c(48.375, 48.5, 48.375, 47, 47.5, 47.35), times=paste(dts, tms))
+#' 
+#' # Create an "uts" with non-numeric observations
+#' uts(list(1:5, c("a", "B")), c("2007-11-08 1:01:00", "2007-11-09 15:16:00"))
+#'
+#' # Create an empty "uts"
+#' uts()
+#'
+uts <- function(values=c(), times=as.POSIXct(character(0)))
+{
+  if (length(values) != length(times))
+    stop("The number of observation values and observation times does not match.")
+  if (!is.POSIXct("POSIXct"))
+    times <- as.POSIXct(times)
+  x <- list(values=values, times=times)
+  class(x) <- c("uts", "list")
+  x   
+}
