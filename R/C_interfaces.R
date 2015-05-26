c36da33ed325bcec0840d3e6f667850d2632f248
Andreas Eckner <andreas@eckner.com>
2015-05-26 16:44:09 -0400

    Verified example

--- R/C_interfaces.R
+++ R/C_interfaces.R
@@ -4,12 +4,12 @@
 
 #' Element Positions of a Sorted Array within Another Sorted Array
 #' 
-#' Assume given two sorted one-dimensional arrays \code{a} and \code{b}. For each element \code{a[i]}, determine the maximum index \code{j} in \code{b} such that \code{b[j] <= a[i]}, (\code{NA} if not found).
+#' Assume given two sorted one-dimensional arrays \code{a} and \code{b}. For each element \code{a[i]}, determine the maximum index \code{j} in \code{b} such that \code{b[j] <= a[i]}, or return \code{NA} if no such index exists.
 #' 
-#' @return A vector of indices of same length as vector \code{a}.
+#' @return An integer vector of indices of same length as \code{a}.
 #' @param a a sorted vector of numbers.
 #' @param b a sorted vector of numbers.
-#' @param eps	tolerance for numerical noise, relative to the largest absolute elements in \code{a} and \code{b}
+#' @param eps	tolerance for numerical noise, relative to the largest absolute element in \code{a} and \code{b}.
 #' 
 #' @examples
 #' sorted_array_position(1:5, 1:5)
@@ -29,18 +29,20 @@ sorted_array_position <- function(a, b, eps=1e-9)
     return(rep(NA, length(a)))
   
   # Convert relative to absolute numerical noise tolerance
-  eps_absolute <- eps * max(a, b, 1, na.rm=TRUE)
+  eps_absolute <- eps * max(abs(a), abs(b), na.rm=TRUE)
+  if (is.na(eps_absolute))
+      return(rep(NA, length(a)))
   
   # Call C function
   res <- integer(length(a))
   res <- .C("sorted_array_position", as.double(a + eps_absolute), as.integer(length(a)), as.double(b),
       as.integer(length(b)), pos = res)$pos
-
+  
   # Set non-found indices to NA 
   res[res == -1] <- NA
   
   # Shift indices by one, because R indicies start at one, while C indices start at zero
-  res + 1
+  as.integer(res + 1)
 }
 
 
