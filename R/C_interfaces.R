45b09b18c22e67abb0d584fe58f10c3dbf2d6a48
Andreas Eckner <andreas@eckner.com>
2015-05-29 13:39:13 -0400

    Replaced sorted_array_position function with num_leq_sorted_arrays

--- R/C_interfaces.R
+++ R/C_interfaces.R
@@ -2,31 +2,44 @@
 # R-C interfaces for helper functions #
 #######################################
 
-#' Element Positions of a Sorted Array within Another Sorted Array
+#' Less-Than-Or-Equal Comparison of Sorted Arrays
 #' 
-#' Assume given two sorted one-dimensional arrays \code{a} and \code{b}. For each element \code{a[i]}, determine the maximum index \code{j} in \code{b} such that \code{b[j] <= a[i]}, or return \code{NA} if no such index exists.
+#' Assume given two sorted one-dimensional arrays \code{a} and \code{b}. For each element \code{a[i]}, determine the number of elements in \code{b} that are less than or equal (leq) to this value.
 #' 
-#' @return An integer vector of indices of same length as \code{a}.
+#' @note
+#' Equivalently, because the input arrays are sorted, for each element \code{a[i]} find the index \code{j} with \code{b[j] <= a[i] < b[j+1]}. 
+#' 
+#' @return A vector of same length as \code{a}.
 #' @param a a sorted vector of numbers.
 #' @param b a sorted vector of numbers.
 #' @param eps	tolerance for numerical noise, relative to the largest absolute element in \code{a} and \code{b}.
 #' 
 #' @examples
-#' sorted_array_position(1:5, 1:5)
-#' sorted_array_position(c(-3, 1, 3, 5, 7), c(0, 1, 4, 9, 16))
+#' # The second vector has
+#' # -) 0 elements leq (less-than-or-equal) -3
+#' # -) 2 elements leq 1
+#' # -) 2 elements leq 3
+#' # -) 3 elements leq 5
+#' # -) 3 elements leq 7
+#' num_leq_sorted_arrays(c(-3, 1, 3, 5, 7), c(0, 1, 4, 9, 16))
 #' 
 #' # Numerical noise < eps has no effect
-#' sorted_array_position(1, c(0, 1))
-#' sorted_array_position(1 - 1e-13, c(0, 1))
-#' sorted_array_position(1 + 1e-13, c(0, 1))
+#' num_leq_sorted_arrays(1, 0:2)
+#' num_leq_sorted_arrays(1 - 1e-13, 0:2)
+#' num_leq_sorted_arrays(1 + 1e-13, 0:2)
 #' 
-sorted_array_position <- function(a, b, eps=1e-9)
+#' # Trivial cases
+#' num_leq_sorted_arrays(1:5, 1:5)
+#' num_leq_sorted_arrays(c(), 1:5)
+#' num_leq_sorted_arrays(1:5, c())
+#' 
+num_leq_sorted_arrays <- function(a, b, eps=1e-12)
 {
   # Trivial cases
   if (length(a) == 0)
-    return(c())
+    return(numeric())
   if (length(b) == 0)
-    return(rep(NA, length(a)))
+    return(rep(0, length(a)))
   
   # Convert relative to absolute numerical noise tolerance
   eps_absolute <- eps * max(abs(a), abs(b), na.rm=TRUE)
@@ -35,14 +48,7 @@ sorted_array_position <- function(a, b, eps=1e-9)
   
   # Call C function
   res <- integer(length(a))
-  res <- .C("sorted_array_position", as.double(a + eps_absolute), as.integer(length(a)), as.double(b),
-      as.integer(length(b)), pos = res)$pos
-  
-  # Set non-found indices to NA 
-  res[res == -1] <- NA
-  
-  # Shift indices by one, because R indicies start at one, while C indices start at zero
-  as.integer(res + 1)
+  .C("num_leq_sorted_arrays", as.double(a + eps_absolute), as.integer(length(a)),
+    as.double(b), as.integer(length(b)), pos = res)$pos
 }
-
 
--- src/utils.c
+++ src/utils.c
@@ -20,22 +20,20 @@
 
 
 /*
- * Assume given two sorted vectors 'a' and 'b'. For each element a[i], determine the maximum index j in
- * 'b' such that b[j] <= a[i], (-1 if not found).
+ * Assume given two sorted vectors 'a' and 'b'. For each element a[i], determine the number of
+ * elements in 'b' than are less than or equal (leq) to this value.
+ *
+ * Equivalently, because the arrays are sorted, for or each element a[i] determine the maximum
+ * index j in 'b' such that b[j-1] <= a[i].
 */
-void sorted_array_position(const double a[], const int *na, const double b[], const int *nb, int pos[])
+void num_leq_sorted_arrays(const double a[], const int *na, const double b[], const int *nb, int pos[])
 {
-  unsigned int i=0, j=0, k=0;
+  unsigned int i=0;
+  int j=-1;
 
-  // Set index to -1 for values of 'a' that are smaller than all values in 'b'
-  while ((k < *na) && (a[k] < b[0])) {
-    pos[k] = -1;
-    k++;
-  }
-
-  for(i = k; i < *na; i++) {
-    while ((j < *nb - 1) && (a[i] >= b[j+1]))
-      j++;
-    pos[i] = j;
+  for(i = 0; i < *na; i++) {
+    while ((j < *nb - 1) && (b[j+1] <= a[i]))
+	  j++;
+    pos[i] = j + 1;
   }
 }
