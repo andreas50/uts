ad2b55572b10cf173ef0068356901fabc2858041
Andreas Eckner <andreas@eckner.com>
2015-06-03 14:50:09 -0400

    Renamed 'eps' to 'tolerance'

--- R/C_interfaces.R
+++ R/C_interfaces.R
@@ -12,7 +12,7 @@
 #' @return A vector of same length as \code{a}.
 #' @param a a sorted vector of numbers.
 #' @param b a sorted vector of numbers.
-#' @param eps tolerance for numerical noise.
+#' @param tolerance tolerance for numerical noise.
 #' 
 #' @examples
 #' # The second vector has
@@ -23,17 +23,17 @@
 #' # -) 3 elements leq 7
 #' num_leq_sorted_arrays(c(-3, 1, 3, 5, 7), c(0, 1, 4, 9, 16))
 #' 
-#' # Numerical noise < eps has no effect
-#' num_leq_sorted_arrays(1, 0:2, eps=1e-12)
-#' num_leq_sorted_arrays(1 - 1e-13, 0:2, eps=1e-12)
-#' num_leq_sorted_arrays(1 + 1e-13, 0:2, eps=1e-12)
+#' # Numerical noise < tolerance has no effect
+#' num_leq_sorted_arrays(1, 0:2, tolerance=1e-12)
+#' num_leq_sorted_arrays(1 - 1e-13, 0:2, tolerance=1e-12)
+#' num_leq_sorted_arrays(1 + 1e-13, 0:2, tolerance=1e-12)
 #' 
 #' # Trivial cases
 #' num_leq_sorted_arrays(1:5, 1:5)
 #' num_leq_sorted_arrays(c(), 1:5)
 #' num_leq_sorted_arrays(1:5, c())
 #' 
-num_leq_sorted_arrays <- function(a, b, eps=0)
+num_leq_sorted_arrays <- function(a, b, tolerance=0)
 {
   # Trivial cases
   if (length(a) == 0)
@@ -47,7 +47,7 @@ num_leq_sorted_arrays <- function(a, b, eps=0)
   
   # Call C function
   res <- integer(length(a))
-  .C("num_leq_sorted_arrays", as.double(a + eps), as.integer(length(a)),
+  .C("num_leq_sorted_arrays", as.double(a + tolerance), as.integer(length(a)),
     as.double(b), as.integer(length(b)), pos = res)$pos
 }
 
