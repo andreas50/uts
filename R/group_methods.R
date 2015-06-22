c920e23e1521e964385cf07a8eaa548f8ee2769b
andreas50 <andreas@eckner.com>
2015-06-22 11:54:33 -0400

    Added Ops.uts

--- R/group_methods.R
+++ R/group_methods.R
@@ -40,69 +40,63 @@ Math.uts <- function(x, ...)
   x
 }
 if (0) {
-  test <- uts1
-  test$values[1] <- NA
-  cumsum(test)
-  cumsum(test, na.rm=T)   # does not work since group generic only supports one argument
-  cumsum(test[!is.na(test)])  # work-around 
   methods("Math")
 }
 
 
-# #' Ops Group Methods for uts
-# #' 
-# #' Apply the \code{\link{Ops}} methods in base \R{} to the observation values of \code{"uts"} objects.
-# #' 
-# #' @param x, x2 \code{"uts"} objects.
-# #' @param \dots further arguments passed to or from methods.
-# #' @param tolerance tolerance for numerical noise in observation times. See \code{\link{sorted_union}}.
-# #' 
-# #' @seealso \code{\link{groupGeneric}}
-# #' 
-# #' @examples
-# #' # Unary oparators
-# #' -ex_uts()
-# #' !ex_uts()
-# #' 
-# #' # Binary operators
-# #' ex_uts() * 2
-# #' ex_uts() + ex_uts()
-# #' ex_uts() > 48
-# Ops.uts <- function(x, x2, tolerance=.Machine$double.eps ^ 0.5)
-# {
-#   # Unary operator
-#   if (missing(x2)) {
-#     x$values <- do.call(.Generic, list(x$values))
-#     return(x)
-#   }
-#   
-#   # Binary operator
-#   if (is.uts(x) & is.uts(x2)) {
-#     # Determine time points of output time series
-#     if (min(length(x), length(x2)) == 0)
-#       return(uts())
-#     if (x$times[1] <= x2$times[1]) {
-#       used_t <- x$times[x$times >= x2$times[1]]
-#       all_times <- sorted_union(used_t, x2$times, eps=1e-9)
-#     } else {
-#       used_t <- x2$times[x2$times >= x$times[1]]
-#       all_times <- sorted_union(x$times, used_t, eps=1e-9)
-#     }
-#     attributes(all_times) <- attributes(x$times)
-#     
-#     # Sample values
-#     values1 <- sample_values(x, all_times)
-#     values2 <- sample_values(x2, all_times)
-#     
-#     # Generate output
-#     out <- uts(do.call(.Generic, list(values1, values2)), all_times)
-#   }  else if (is(x2, "numeric") | is(x2, "integer")) {
-#     out <- x
-#     out$values <- do.call(.Generic, list(x$values,  x2))
-#   } else {
-#     out <- x2
-#     out$values <- do.call(.Generic, list(x,  x2$values))
-#   }
-#   out
-# }
+#' Ops Group Methods for uts
+#' 
+#' Apply the \code{\link{Ops}} methods in base \R{} to the observation values of \code{"uts"} objects.
+#' 
+#' @note For unary oparations, the output time series has the same observation times as the input time series.
+#' @note For binary operations involving two time series \code{uts1} and \code{uts2}, the output time series has the union of observation times of \code{uts1} and \code{uts2}, but excluding times before \emph{both} time series have their initial observation. The method for determining these times is unaffected by numerical noise less than \code{sqrt(\link[=.Machine]{.Machine$double.eps})}.
+#' 
+#' @param e1,e2 either \code{"uts"} objects or numeric numbers.
+#' @param \dots further arguments passed to or from methods.
+#' 
+#' @seealso \code{\link{groupGeneric}}
+#' 
+#' @examples
+#' # Unary oparators
+#' -ex_uts()
+#' !ex_uts()
+#' 
+#' # Binary operators
+#' ex_uts() * 2
+#' 2 * ex_uts()
+#' ex_uts() / ex_uts()
+#' ex_uts() > 48
+#' 48 >= ex_uts()
+Ops.uts <- function(e1, e2)
+{
+  # Unary operator
+  if (missing(e2)) {
+    e1$values <- do.call(.Generic, list(e1$values))
+    return(e1)
+  }
+  
+  # Binary operator
+  if (is.uts(e1) & is.uts(e2)) {
+    # Determine time points of output time series
+    if (min(length(e1), length(e2)) == 0)
+      return(uts())
+    all_times <- sorted_union(e1$times, e2$times, tolerance=.Machine$double.eps ^ 0.5)
+    all_times <- all_times[all_times >= min(e1$times[1], e2$times[2])]
+    attributes(all_times) <- attributes(e1$times)
+    
+    # Sample values
+    values1 <- sample_values(e1, all_times)
+    values2 <- sample_values(e2, all_times)
+    
+    # Generate output
+    out <- uts(do.call(.Generic, list(values1, values2)), all_times)
+  }  else if (is(e2, "numeric") | is(e2, "integer")) {
+    out <- e1
+    out$values <- do.call(.Generic, list(e1$values,  e2))
+  } else {
+    out <- e2
+    out$values <- do.call(.Generic, list(e1,  e2$values))
+  }
+  out
+}
 
