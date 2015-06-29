b701505ddf7d349d17fa3a639fee5ad72765eaf3
andreas50 <andreas@eckner.com>
2015-06-29 10:52:10 -0400

    Added is.na.uts()

---
+++ R/NAs.R
@@ -0,0 +1,46 @@
+###################
+# Handling of NAs #
+###################
+
+#' Remove NAs
+#' 
+#' Returns the object with incomplete cases removed.
+#' 
+#' @param object a time series object.
+#' @param \dots further arguments passed to or from methods.
+#' 
+#' @seealso \code{\link[stats:na.fail]{na.fail}}, \code{\link[stats:na.fail]{na.omit}}
+#' @examples
+#' # Remove NAs from a "uts"
+#' tmp <- ex_uts()
+#' tmp$values[c(2, 4)] <- NA
+#' na.omit(tmp)
+na.omit.uts <- function(object, ...)
+{
+  keep <- !is.na(object$values)
+  object$values <- object$values[keep]
+  object$times <- object$times[keep]
+  object
+}
+
+
+#' Not Available / Missing Values
+#' 
+#' Find missing values.
+#' 
+#' \code{is.na} returns a logical \code{"uts"}, i.e. a \code{"uts"} with \code{\link{logical}} observation values, indication which observation values are \code{NA}.
+#' 
+#' @param x a \code{"uts"} object.
+#' 
+#' @seealso \code{\link{na.omit}}
+#' @examples
+#' # Set observation to NA
+#' test <- ex_uts()
+#' test$values[c(2, 4)] <- NA
+#' 
+#' # Get logical "uts", indicating which observations are NA
+#' is.na(test)
+is.na.uts <- function(x)
+{
+  uts(is.na(x$values), x$times)
+}
