5e22730b73518e76c5901ef02e91381d1df6836a
andreas50 <andreas@eckner.com>
2015-07-08 13:41:20 -0400

    Improved documentation

--- R/NAs.R
+++ R/NAs.R
@@ -9,7 +9,8 @@
 #' @param object a time series object.
 #' @param \dots further arguments passed to or from methods.
 #' 
-#' @seealso \code{\link[is.na.uts]{is.na}}, \code{\link[stats:na.fail]{na.fail}}, \code{\link[stats:na.fail]{na.omit}}
+#' @seealso \code{\link[base:is.na]{is.na}}, \code{\link[stats:na.fail]{na.fail}}, \code{\link[stats:na.fail]{na.omit}} in base \R.
+#' @seealso \code{\link{is.na}} for \code{"uts"} objects.
 #' @examples
 #' # Remove NAs from a "uts"
 #' tmp <- ex_uts()
@@ -30,8 +31,8 @@ na.omit.uts <- function(object, ...)
 #' 
 #' @return A logical \code{"uts"} (i.e. a \code{"uts"} with \code{\link{logical}} observation values), indicating which observation values are \code{NA}.
 #' @param x a \code{"uts"} object.
-#' 
-#' @seealso \code{\link[na.omit.uts]{na.omit}}
+#' @seealso \code{\link[base:is.na]{is.na}}, \code{\link[stats:na.fail]{na.fail}}, \code{\link[stats:na.fail]{na.omit}} in base \R.
+#' @seealso \code{\link{na.omit}} for \code{"uts"} objects.
 #' @examples
 #' # Set observation to NA
 #' test <- ex_uts()
