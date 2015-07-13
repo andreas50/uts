e8bb01e7c1e6a200e2595c21d6353f5bd40b940b
andreas50 <andreas@eckner.com>
2015-07-13 09:55:44 -0400

    renamed "business_day" to "weekday"

--- R/POSIXt_extensions.R
+++ R/POSIXt_extensions.R
@@ -6,34 +6,34 @@
 # Generic functions
 # -----------------
 
-#' Next Business Day
+#' Next Weekday
 #'
-#' For objects representing dates, find the next business day.
+#' For objects representing dates, find the next weekday.
 #' 
-#' Add the minimum number of days to reach a business day. The time-of-day information is therefore unaffected. In particular, if the input already is a business day, then the output is the same as the input.
-#'
-#' @return An object of the same class and length as the input \code{x}
-#' @param x an object representing dates.
-#' @param \dots further arguments passed to or from methods.
-#' 
-#' @keywords chron
-#' @seealso \code{\link{previous_business_day}}
-next_business_day <- function(x, ...) UseMethod("next_business_day")
-
-
-#' Previous Business Day
-#' 
-#' For objects representing dates, find the previous business day.
-#'
-#' Substract the minimum number of days to reach a business day. The time-of-day information is therefore unaffected. In particular, if the input already is a business day, then the output is the same as the input.
+#' Add the minimum number of days to reach a weekday (Monday-Friday). The time-of-day information is therefore unaffected. In particular, if the input already is a weekday, then the output is the same as the input.
 #'
 #' @return An object of the same class and length as the input \code{x}
 #' @param x an object representing dates.
 #' @param \dots further arguments passed to or from methods.
 #' 
 #' @keywords chron
-#' @seealso \code{\link{next_business_day}}
-previous_business_day <- function(x, ...) UseMethod("previous_business_day")
+#' @seealso \code{\link{previous_weekday}}
+next_weekday <- function(x, ...) UseMethod("next_weekday")
+
+
+#' Previous Weekday
+#' 
+#' For objects representing dates, find the previous weekday.
+#'
+#' Substract the minimum number of days to reach a weekday (Monday-Friday). The time-of-day information is therefore unaffected. In particular, if the input already is a weekday, then the output is the same as the input.
+#'
+#' @return An object of the same class and length as the input \code{x}
+#' @param x an object representing dates.
+#' @param \dots further arguments passed to or from methods.
+#' 
+#' @keywords chron
+#' @seealso \code{\link{next_weekday}}
+previous_weekday <- function(x, ...) UseMethod("previous_weekday")
 
 
 # ----------------------
@@ -41,14 +41,12 @@ previous_business_day <- function(x, ...) UseMethod("previous_business_day")
 # ----------------------
 
 
-#' @describeIn next_business_day find the next business day for POSIXt dates
-#' 
-#' @note For \code{next_business_day.POSIXt()}, non-business days are defined as Saturdays and Sundays. Public holidays are not supported.
+#' @describeIn next_weekday find the next weekday for POSIXt dates
 #' 
 #' @examples
 #' # 2015-04-20 is a Monday
-#' next_business_day(as.POSIXct("2015-04-20") + days(0:6))
-next_business_day.POSIXt <- function(x, ...)
+#' next_weekday(as.POSIXct("2015-04-20") + days(0:6))
+next_weekday.POSIXt <- function(x, ...)
 {
   day_of_week <- wday(x)
   x[day_of_week == 7] <- x[day_of_week == 7] + days(2)    # update Saturdays
@@ -57,14 +55,12 @@ next_business_day.POSIXt <- function(x, ...)
 }
 
 
-#' @describeIn previous_business_day find the previous business day for POSIXt dates
-#' 
-#' @note For \code{previous_business_day.POSIXt()}, non-business days are defined as Saturdays and Sundays. Public holidays are not supported.
+#' @describeIn previous_weekday find the previous weekday for POSIXt dates
 #' 
 #' @examples
 #' # 2015-04-20 is a Monday
-#' previous_business_day(as.POSIXct("2015-04-20") + days(0:6))
-previous_business_day.POSIXt <- function(x, ...)
+#' previous_weekday(as.POSIXct("2015-04-20") + days(0:6))
+previous_weekday.POSIXt <- function(x, ...)
 {
   day_of_week <- wday(x)
   x[day_of_week == 7] <- x[day_of_week == 7] - days(1)    # update saturdays
