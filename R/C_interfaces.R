7415a3e9fc3fff10069bf5d126467f836d8089ed
andreas50 <andreas@eckner.com>
2015-07-25 10:54:04 -0400

    Renamed all "array" instances to "vector"

--- R/C_interfaces.R
+++ R/C_interfaces.R
@@ -2,12 +2,12 @@
 # R-C interfaces for helper functions #
 #######################################
 
-#' Less-Than-Or-Equal Comparison of Sorted Arrays
+#' Less-Than-Or-Equal Comparison of Sorted Vectors
 #' 
-#' Assume given two sorted one-dimensional arrays \code{a} and \code{b}. For each element \code{a[i]}, determine the number of elements in \code{b} that are less than or equal (leq) to this value.
+#' Assume given two sorted numeric vectors \code{a} and \code{b}. For each element \code{a[i]}, determine the number of elements in \code{b} that are less than or equal (leq) to this value.
 #' 
 #' @note
-#' Equivalently, because the input arrays are sorted, for each element \code{a[i]} determine the maximum index \code{j} with \code{b[j] <= a[i]}. 
+#' Equivalently, because the input vectors are sorted, for each element \code{a[i]} determine the maximum index \code{j} with \code{b[j] <= a[i]}. 
 #' 
 #' @return A vector of same length as \code{a}.
 #' @param a a sorted vector of numbers.
@@ -22,18 +22,18 @@
 #' # -) 2 elements leq 3
 #' # -) 3 elements leq 5
 #' # -) 3 elements leq 7
-#' num_leq_sorted_arrays(c(-3, 1, 3, 5, 7), c(0, 1, 4, 9, 16))
+#' num_leq_sorted(c(-3, 1, 3, 5, 7), c(0, 1, 4, 9, 16))
 #' 
 #' # Numerical noise < tolerance has no effect
-#' num_leq_sorted_arrays(1, 0:2, tolerance=1e-12)
-#' num_leq_sorted_arrays(1 - 1e-13, 0:2, tolerance=1e-12)
-#' num_leq_sorted_arrays(1 + 1e-13, 0:2, tolerance=1e-12)
+#' num_leq_sorted(1, 0:2, tolerance=1e-12)
+#' num_leq_sorted(1 - 1e-13, 0:2, tolerance=1e-12)
+#' num_leq_sorted(1 + 1e-13, 0:2, tolerance=1e-12)
 #' 
 #' # Trivial cases
-#' num_leq_sorted_arrays(1:5, 1:5)
-#' num_leq_sorted_arrays(c(), 1:5)
-#' num_leq_sorted_arrays(1:5, c())
-num_leq_sorted_arrays <- function(a, b, tolerance=0)
+#' num_leq_sorted(1:5, 1:5)
+#' num_leq_sorted(c(), 1:5)
+#' num_leq_sorted(1:5, c())
+num_leq_sorted <- function(a, b, tolerance=0)
 {
   # Trivial cases
   if (length(a) == 0)
@@ -47,7 +47,7 @@ num_leq_sorted_arrays <- function(a, b, tolerance=0)
   
   # Call C function
   res <- integer(length(a))
-  .C("num_leq_sorted_arrays", as.double(a + tolerance), as.integer(length(a)),
+  .C("num_leq_sorted", as.double(a + tolerance), as.integer(length(a)),
     as.double(b), as.integer(length(b)), pos = res)$pos
 }
 
@@ -55,7 +55,7 @@ num_leq_sorted_arrays <- function(a, b, tolerance=0)
 
 #' Sorted Union
 #' 
-#' For two sorted one-dimensional arrays \code{a} and \code{b}, determine the sorted union of elements.
+#' For two sorted numeric vectors \code{a} and \code{b}, determine the sorted union of elements.
 #' 
 #' @return A vector of same length as \code{a}.
 #' @param a a sorted vector of numbers.
