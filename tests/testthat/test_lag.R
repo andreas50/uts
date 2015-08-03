0a94a702fa733ee06e16cff5011a5a9de681e038
andreas50 <andreas@eckner.com>
2015-08-03 12:28:01 -0400

    Added lag() and lag_t() tests

---
+++ tests/testthat/test_lag.R
@@ -0,0 +1,30 @@
+context("lag")
+
+test_that("lag works",{
+  # Trivial case
+  expect_equal(
+    lag(ex_uts(), k=6),
+    uts()
+  )
+  
+  # Regression tests
+  expect_equal_to_reference(lag(ex_uts(), k=1), file="test_lag_1.rds")
+  expect_equal_to_reference(lag(ex_uts(), k=-2), file="test_lag_minus_2.rds")
+})
+
+
+test_that("lag_t works",{
+  # Trivial case
+  expect_equal(
+    lag_t(ex_uts(), ddays(0)),
+    ex_uts()
+  )
+  
+  # Intentional error
+  expect_error(
+    lag_t(ex_uts(), 3)
+  )
+  
+  # Regression tests
+  expect_equal_to_reference(lag_t(ex_uts(), dhours(7.5)), file="test_lag_t.rds")
+})
