afabb775b5188ea94f4915cb921e98f8468e472c
andreas50 <andreas@eckner.com>
2015-08-17 09:19:33 -0400

    Fixed typo

--- R/uts.R
+++ R/uts.R
@@ -17,7 +17,7 @@
 #' tms <- c("7:00:00", "8:01:00", "13:15:00", "7:30:00", "8:51:00", "15:15:00")
 #' uts(values=c(48.375, 48.5, 48.375, 47, 47.5, 47.35), times=paste(dts, tms))
 #' 
-#' # Create an "uts" with non-numeric observations
+#' # Create a "uts" with non-numeric observations
 #' uts(list(1:5, c("a", "B")), c("2007-11-08 1:01:00", "2007-11-09 15:16:00"))
 #'
 #' # Create an empty "uts"
