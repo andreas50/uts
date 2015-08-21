d56ebe806b2ed5684f0ab9aea82acbcda05089a1
andreas50 <andreas@eckner.com>
2015-08-21 12:48:01 -0400

    Fixed na.omit() bug and added na.omit() and is.na() tests

---
+++ tests/testthat/test-NAs.R
@@ -0,0 +1,38 @@
+context("NA")
+
+test_that("na.omit works",{
+  # Regression tests
+  tmp <- ex_uts()
+  tmp$values[c(2, 4)] <- NA
+  expect_equal_to_reference(na.omit(tmp), file="test-NAs_1.rds")
+  
+  # Same time series returned if no NAs present
+  expect_equal(
+    na.omit(ex_uts()),
+    ex_uts()
+  )
+  expect_equal(
+    na.omit(uts()),
+    uts()
+  )
+})
+
+
+test_that("is.na works",{
+  tmp <- ex_uts()
+  tmp$values[c(2, 4)] <- NA
+  expect_equal(
+    which(is.na(tmp)),
+    tmp$times[c(2, 4)]
+  )
+  
+  # Empty POSIXct object returned if no NAs present
+  expect_equal(
+    which(is.na(ex_uts())),
+    as.POSIXct(character())
+  )
+  expect_equal(
+    which(is.na(uts())),
+    as.POSIXct(character())
+  )
+})
