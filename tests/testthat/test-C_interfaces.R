4fb3602ccd45183d1a3363f111d6a0888ce66beb
andreas50 <andreas@eckner.com>
2015-09-09 08:24:12 -0400

    Added sorted_union tests

--- tests/testthat/test-C_interfaces.R
+++ tests/testthat/test-C_interfaces.R
@@ -16,6 +16,11 @@ test_that("num_leq_sorted robust to numerical noise",{
     num_leq_sorted(1 + 1e-13, 0:2, tolerance=1e-12),
     2L
   )
+  
+  expect_identical(
+    num_leq_sorted(seq(to=1, by=0.8e-10, length=3), 1, tolerance=1e-10),
+    as.integer(c(0, 1, 1))
+  )
 })
 
 test_that("num_leq_sorted correctly handles trivial cses",{
@@ -32,3 +37,46 @@ test_that("num_leq_sorted correctly handles trivial cses",{
     rep(0L, 5)
   )
 })
+
+
+
+test_that("sorted_union works",{
+  expect_identical(
+    sorted_union(1:3, 2:4),
+    as.numeric(1:4)
+  )
+  expect_identical(
+    sorted_union(c(), 1:10),
+    as.numeric(1:10)
+  )
+  expect_identical(
+    sorted_union(1:10, c()),
+    as.numeric(1:10)
+  )
+})
+
+test_that("sorted_union robust to numerical noise",{
+  expect_identical(
+    sorted_union(0, 1e-14),
+    c(0, 1e-14)
+  )
+  expect_identical(
+    sorted_union(0, 1e-14, tolerance=1e-12),
+    as.numeric(0)
+  )
+  expect_identical(
+    sorted_union(c(0, 1e-14), 2, tolerance=1e-12),
+    as.numeric(c(0, 2))
+  )
+  
+  expect_identical(
+    sorted_union(seq(0, 2, by=0.5), c(), tolerance=1),
+    c(0, 1.5)
+  )
+  expect_identical(
+    sorted_union(0, 1, tolerance=1),
+    as.numeric(0)
+  )
+})
+
+
