ead5d86cfd06c397aa57b3a8c2f9bd0958170186
andreas50 <andreas@eckner.com>
2016-02-27 21:09:50 -0500

    Fix tests

--- tests/testthat/test-coercions.R
+++ tests/testthat/test-coercions.R
@@ -15,17 +15,17 @@ test_that("multivariate time series are not converted",{
   
   # irts
   if (requireNamespace("tseries", quietly = TRUE)) {
-    mat <- structure(1:6, dim=c(2,3), dimnames=list(c("2003-01-01","2003-01-04"), letters[1:3]))
-    its1 <- its::its(mat)
-    expect_error(as.uts(its1))
-  }
-  
-  # its
-  if (requireNamespace("its", quietly = TRUE)) {
     times <- cumsum(rexp(10, rate = 0.1))
     values <- matrix(rnorm(20), nrow=10)
     x <- tseries::irts(times, values)
     expect_error(as.uts(x))
+  }
+
+  # its
+  if (requireNamespace("its", quietly = TRUE)) {
+    mat <- structure(1:6, dim=c(2,3), dimnames=list(c("2003-01-01","2003-01-04"), letters[1:3]))
+    its1 <- its::its(mat)
+    expect_error(as.uts(its1))
   }
   
   # xts
