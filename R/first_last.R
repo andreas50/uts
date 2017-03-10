68336627a702f990a75feb81a09ae73948baa629
Andreas Eckner <andreas@eckner.com>
2017-03-10 13:20:12 -0500

    Add first.default and last.default

--- R/first_last.R
+++ R/first_last.R
@@ -14,6 +14,20 @@
 first <- function(x, ...) UseMethod("first")
 
 
+#' @describeIn first Assume that \code{x} is a vector or compatible object and return the first value.
+#' 
+#' @examples
+#' first(5:10)
+#' first(list(a="test", b=3))
+first.default <- function(x, ...)
+{
+  if (length(x) > 0)
+    x[[1]]
+  else
+    NULL
+}
+
+
 #' @describeIn first Return the first observation \emph{value} of a \code{"uts"} object. Unlike method \code{first()} in package \href{https://cran.r-project.org/web/packages/xts/}{xts}, arguments \code{n} and \code{keep} are not supported. Instead, this functionality is provided by \code{head()} and \code{heat_t()}.
 #' 
 #' @examples
@@ -37,6 +51,20 @@ first.uts <- function(x, ...)
 #' @seealso \code{\link{first}}
 #' @seealso \code{\link{start}} and \code{\link{end}} return, respectively, the first and last obvservation \emph{time} from a time series.
 last <- function(x, ...) UseMethod("last")
+
+
+#' @describeIn last Assume that \code{x} is a vector or compatible object and return the last value.
+#' 
+#' @examples
+#' last(5:10)
+#' last(list(a="test", b=3))
+last.default <- function(x, ...)
+{
+  if (length(x) > 0)
+    x[[length(x)]]
+  else
+    NULL
+}
 
 
 #' @describeIn last Return the last observation \emph{value} of a \code{"uts"} object. Unlike method \code{last()} in package \href{https://cran.r-project.org/web/packages/xts/}{xts}, arguments \code{n} and \code{keep} are not supported. Instead, this functionality is provided by \code{tail()} and \code{tail_t()}.
--- tests/testthat/test-first_last.R
+++ tests/testthat/test-first_last.R
@@ -1,6 +1,50 @@
 context("first_last")
 
-test_that("first works",{
+test_that("first.default works",{
+  expect_identical(
+    first(5:10),
+    5L
+  )
+  expect_identical(
+    first(list(a="test", b=3)),
+    "test"
+  )
+  
+  # Empty array, list
+  expect_identical(
+    first(c()),
+    NULL
+  )
+  expect_identical(
+    first(list()),
+    NULL
+  )
+})
+
+
+test_that("last.default works",{
+  expect_identical(
+    last(5:10),
+    10L
+  )
+  expect_identical(
+    last(list(a="test", b=3)),
+    3
+  )
+  
+  # Empty array, list
+  expect_identical(
+    last(c()),
+    NULL
+  )
+  expect_identical(
+    last(list()),
+    NULL
+  )
+})
+
+
+test_that("first.uts works",{
   expect_identical(
     first(ex_uts()),
     ex_uts()$values[1]
@@ -17,7 +61,7 @@ test_that("first works",{
   )
 })
 
-test_that("last works",{
+test_that("last.uts works",{
   expect_identical(
     last(ex_uts()),
     ex_uts()$values[length(ex_uts())]
